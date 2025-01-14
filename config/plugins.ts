33e42c6a9e6263b43678becb644ead1bf6d5afa4
lzamranoa <lzambranoa.software@gmail.com>
2025-01-13 22:50:36 -0500

    ajuste de archivo plugins con configuración de smtp para mail

--- config/plugins.ts
+++ config/plugins.ts
@@ -4,6 +4,22 @@ export default ({ env }) => ({
             jwtSecret: env('JWT_SECRET'),
         },
     },
+    email: {
+        provider: 'nodemailer',  // Cambia según el proveedor que uses
+        providerOptions: {
+          host: env('SMTP_HOST', 'mail.ecopartyproducciones.com'),
+          port: env('SMTP_PORT', 465),
+          secure: true,
+          auth: {
+            user: env('SMTP_USER', 'tu-email@empresa.com'),
+            pass: env('SMTP_PASS', 'tu-contraseña'),
+          },
+        },
+        settings: {
+          defaultFrom: 'info@ecopartyproducciones.com',
+          defaultReplyTo: 'info@ecopartyproducciones.com',
+        },
+      },
     upload: {
         config: {
             provider: 'cloudinary',
